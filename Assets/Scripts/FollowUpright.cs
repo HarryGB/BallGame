2e2a9966dcc5610005aa80bc0b090cc3a59c2e1f
HarryGB <harry.ellem94@gmail.com>
2021-06-26 19:06:43 +1000

    shadows are fucked, going to redo in unreal

---
+++ Assets/Scripts/FollowUpright.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUpright : MonoBehaviour
+{
+    public Transform parent;
+    private Transform self;
+
+    private void Start()
+    {
+        self = gameObject.transform;
+    }
+    void FixedUpdate()
+    {
+        self.position = parent.position;
+    }
+}
