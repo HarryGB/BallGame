b00c99640117d10b476b79f55807978d6481008d
HarryGB <harry.ellem94@gmail.com>
2021-07-24 16:21:09 +1000

    added sort of bobbing, not quite right though

---
+++ Assets/Scripts/FlameBobbing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameBobbing : MonoBehaviour
+{
+    public Rigidbody external;
+    public float speed;
+    public float scale;
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(external.angularVelocity.x * scale, external.angularVelocity.y * scale, external.angularVelocity.z * scale), speed);
+    }
+}
